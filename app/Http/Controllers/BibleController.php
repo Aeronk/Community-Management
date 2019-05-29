6b772fe2f5cd5bf3262b91c8d1be5b5c30669825
ernest <chikuwaernest@gmail.com>
2019-05-29 17:01:07 +0200

    This removes some spaces

--- app/Http/Controllers/BibleController.php
+++ app/Http/Controllers/BibleController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
 use App\Bible;
 use Illuminate\Http\Request;
@@ -14,8 +15,8 @@ class BibleController extends Controller
      */
     public function index()
     {
-        $bibles=Bible::all();
-        return view('bible.bible-view')->with('bibles',$bibles);
+        $bibles = Bible::all();
+        return view('bible.bible-view')->with('bibles', $bibles);
     }
 
     /**
@@ -25,53 +26,48 @@ class BibleController extends Controller
      */
     public function create()
     {
-        $courses=DB::table('courses')->get();
-        return view('bible.bible-add')->with('courses',$courses);
+        $courses = DB::table('courses')->get();
+        return view('bible.bible-add')->with('courses', $courses);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
 
-         $this->validate($request, [
+        $this->validate($request, [
             'name' => 'required',
             'enrollment' => 'numeric',
             'number_of_branches' => 'numeric',
             'sub_balance' => 'numeric'
-          
-         
-            
         ]);
 
-
-            $string3='';
-             if (is_array($request->input('courses')) || is_object($request->input('courses')))
-                {
-             foreach ($request->input('courses') as $course){
-                $string3 .=  $course.',';
-             }
-                 }
+        $string3 = '';
+        if (is_array($request->input('courses')) || is_object($request->input('courses'))) {
+            foreach ($request->input('courses') as $course) {
+                $string3 .= $course . ',';
+            }
+        }
 
         Bible::unguard();
-        $bible=Bible::create([
+        $bible = Bible::create([
 
-        'name'=>$request->input('name'),
-        'year'=> $request->input('year'),
-        'enrollment'=> $request->input('enrollment'),
-        'address' => $request->input('address'),
-        'mobile' => $request->input('mobile'),
-        'email' => $request->input('email'),
-        'founder' => $request->input('founder'),
-        'subscription' => $request->input('subscription'),
-        'courses' => $string3,
-        'sub_balance' => $request->input('sub_balance'),
-        'number_of_branches' => $request->input('number_of_branches'),
-        'category' => $request->input('category')
+            'name' => $request->input('name'),
+            'year' => $request->input('year'),
+            'enrollment' => $request->input('enrollment'),
+            'address' => $request->input('address'),
+            'mobile' => $request->input('mobile'),
+            'email' => $request->input('email'),
+            'founder' => $request->input('founder'),
+            'subscription' => $request->input('subscription'),
+            'courses' => $string3,
+            'sub_balance' => $request->input('sub_balance'),
+            'number_of_branches' => $request->input('number_of_branches'),
+            'category' => $request->input('category')
 
         ]);
 
@@ -82,7 +78,7 @@ class BibleController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Bible  $bible
+     * @param  \App\Bible $bible
      * @return \Illuminate\Http\Response
      */
     public function show(Bible $bible)
@@ -93,50 +89,49 @@ class BibleController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Bible  $bible
+     * @param  \App\Bible $bible
      * @return \Illuminate\Http\Response
      */
     public function edit(Bible $bible)
     {
-        $courses=DB::table('courses')->get();
-       return view('bible.bible-edit')->with('bible', $bible)
-                                      ->with('courses', $courses);
+        $courses = DB::table('courses')->get();
+        return view('bible.bible-edit')->with('bible', $bible)
+            ->with('courses', $courses);
 
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Bible  $bible
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Bible $bible
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Bible $bible)
     {
 
-           $string3='';
-             if (is_array($request->input('courses')) || is_object($request->input('courses')))
-                {
-             foreach ($request->input('courses') as $course){
-                $string3 .=  $course.',';
-             }
-                 }
+        $string3 = '';
+        if (is_array($request->input('courses')) || is_object($request->input('courses'))) {
+            foreach ($request->input('courses') as $course) {
+                $string3 .= $course . ',';
+            }
+        }
 
-         Bible::unguard();
+        Bible::unguard();
         $bible->update([
 
-        'name'=>$request->input('name'),
-        'year'=> $request->input('year'),
-        'enrollment'=> $request->input('enrollment'),
-        'address' => $request->input('address'),
-        'mobile' => $request->input('mobile'),
-        'email' => $request->input('email'),
-        'founder' => $request->input('founder'),
-        'subscription' => $request->input('subscription'),
-        'courses' => $string3,
-        'sub_balance' => $request->input('sub_balance'),
-        'number_of_branches' => $request->input('number_of_branches'),
-        'category' => $request->input('category')
+            'name' => $request->input('name'),
+            'year' => $request->input('year'),
+            'enrollment' => $request->input('enrollment'),
+            'address' => $request->input('address'),
+            'mobile' => $request->input('mobile'),
+            'email' => $request->input('email'),
+            'founder' => $request->input('founder'),
+            'subscription' => $request->input('subscription'),
+            'courses' => $string3,
+            'sub_balance' => $request->input('sub_balance'),
+            'number_of_branches' => $request->input('number_of_branches'),
+            'category' => $request->input('category')
 
         ]);
 
@@ -147,16 +142,16 @@ class BibleController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Bible  $bible
+     * @param  \App\Bible $bible
      * @return \Illuminate\Http\Response
      */
     public function destroy(Bible $bible)
     {
-           if($bible->delete()) {
+        if ($bible->delete()) {
             return redirect()->back()->with('message', "Record deleted successfully");
         }
 
         return redirect()->back()->with('error', "An error occured while deleting record");
-    
+
     }
 }
