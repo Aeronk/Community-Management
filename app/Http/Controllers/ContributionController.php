18656f1fe7cab009f81b47a248a03f90928f42cc
ernest <chikuwaernest@gmail.com>
2019-06-05 12:50:30 +0200

    This fixes error when saving a contribution

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
-use App\contribution;
+use App\Contribution;
 use App\category;
 
 use App\Minister;
@@ -20,10 +21,10 @@ class ContributionController extends Controller
      */
     public function index()
     {
-       
-        $contris=Contribution::all();
-        $types=ContributionType::all();
-        return view('finance.viewcontributions')->with('contris',$contris)->with('types',$types);
+
+        $contris = Contribution::all();
+        $types = ContributionType::all();
+        return view('finance.viewcontributions')->with('contris', $contris)->with('types', $types);
     }
 
     /**
@@ -34,70 +35,72 @@ class ContributionController extends Controller
     public function create()
 
     {
-        $denominations=Denomination::with('category')->get();
-        $ministers=Minister::all();
-        $contributions=DB::table('contribution_types')->get();
-        $provinces=DB::table('provinces')->get();
-        $zones=DB::table('regions')->get();
-        return view('finance.addcontribution')->with('denominations',$denominations)->with('contributions',$contributions)->with('ministers',$ministers)->with('provinces',$provinces)->with('zones',$zones);
+        $denominations = Denomination::with('category')->get();
+        $ministers = Minister::all();
+        $contributions = DB::table('contribution_types')->get();
+        $provinces = DB::table('provinces')->get();
+        $zones = DB::table('regions')->get();
+        return view('finance.addcontribution')->with('denominations', $denominations)->with('contributions', $contributions)->with('ministers', $ministers)->with('provinces', $provinces)->with('zones', $zones);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-             Contribution::unguard();
-          $indcontributions=Contribution::create([
+//        return $request->all();
+        Contribution::unguard();
 
-            'denomination_id' =>$request->input('denomination_id'),
-            'province' =>$request->input('province'),
-            'zone' =>$request->input('zone'),
+        Contribution::create([
+
+            'denomination_id' => $request->input('denomination_id'),
+            'province' => $request->input('province'),
+            'zone' => $request->input('zone'),
             'type' => $request->input('type'),
             'transaction_id' => $request->input('transaction_id'),
-            'amount' =>$request->input('amount_recieved'),
-            'received_from' =>$request->input('received_from'),
-            'date_recieved' =>$request->input('date_recieved'),
-            'status' =>$request->input('status'),
-            'comment' =>$request->input('comment')
-       ] );
+            'amount' => $request->input('amount_recieved'),
+            'received_from' => $request->input('received_from'),
+            'date_recieved' => $request->input('date_recieved'),
+            'status' => $request->input('status'),
+            'comment' => $request->input('comment')
+        ]);
 
-           return redirect()->back()->with('message', 'Contribution Added successfully');
-             
+        return redirect()->back()->with('message', 'Contribution Added successfully');
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\contribution  $contribution
+     * @param  \App\contribution $contribution
      * @return \Illuminate\Http\Response
      */
     public function show(contribution $contribution)
 
     {
-        return view('finance.singlecontri')->with('contribution',$contribution);
+        return view('finance.singlecontri')->with('contribution', $contribution);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\contribution  $contribution
+     * @param  \App\contribution $contribution
      * @return \Illuminate\Http\Response
      */
     public function edit(contribution $contribution)
     {
-        return view('finance.editcontri')->with('contribution',$contribution);
+        return view('finance.editcontri')->with('contribution', $contribution);
         //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\contribution  $contribution
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\contribution $contribution
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, contribution $contribution)
@@ -108,7 +111,7 @@ class ContributionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\contribution  $contribution
+     * @param  \App\contribution $contribution
      * @return \Illuminate\Http\Response
      */
     public function destroy(contribution $contribution)
