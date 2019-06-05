10d205b17d25d8eca52b20aa7e28aab022a184f8
ernest <chikuwaernest@gmail.com>
2019-06-05 13:14:54 +0200

    This adds Back to Listing button  when you view a record after adding it

--- resources/views/finance/singlecontri.blade.php
+++ resources/views/finance/singlecontri.blade.php
@@ -15,46 +15,68 @@ Denominations
 @section('content')
     <div class="am-content">
     <div class="main-content">
+		<div class="container">
+			<div class="row">
 
-<table class="table table-striped table-bordered">
-	<thead>
-		<tr class="">
-<!--th width="40%"><h2></h2></th>
-	<th width="60%"><h2></h2></th-->
-	</tr>
-</thead>  
-<tbody>   
-	<tr>
-		<td><b style="font-family:serif;font-size:15px">Ministry</b></td>
-		<td>{{$individualContribution->denomination_id}}</td>
-	</tr> 
-	<tr>
-		<td><b style="font-family:serif;font-size:15px">Received From</b></td>
-		<td>Singazivi Vengesayi</td></tr> <tr><td><b style="font-family:serif;font-size:15px">Contribution Type</b></td><td>Offering</td>
-		</tr>
-
-		<tr><td><b style="font-family:serif;font-size:15px">Amount</b></td><td>200.00</td>
-		</tr>
-		<tr><td><b style="font-family:serif;font-size:15px">Date Income</b></td><td>2018-04-15</td>
-		</tr>
-		<tr>
-			<td><b style="font-family:serif;font-size:15px">Captured By</b></td><td>Mashingaidze</td>
-		</tr>
-		<tr>
-			<td><b style="font-family:serif;font-size:15px">Date Captured</b></td>
-			<td>2018-04-16 03:49:20</td></tr> <tr><td><b style="font-family:serif;font-size:15px">Administrator</b></td>
-				<td>Mashingaidze</td>
-			</tr>
-		<tr>
-			<td><b style="font-family:serif;font-size:15px">Comment</b></td>
-			<td></td>
-		</tr>                     
+				<a href="{{ route('contribution.index') }}" class="btn btn-sm btn-primary"><i class="fa fa-hand"></i>Back
+					to listing</a><br>
 
 
-		</tbody>                 
+			</div>
+			<div class="row">
+				<table class="table table-striped table-bordered">
+					<thead>
+					<tr class="">
+						<!--th width="40%"><h2></h2></th>
+                            <th width="60%"><h2></h2></th-->
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Ministry</b></td>
+						<td>{{$individualContribution->denomination_id}}</td>
+					</tr>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Received From</b></td>
+						<td>Singazivi Vengesayi</td>
+					</tr>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Contribution Type</b></td>
+						<td>Offering</td>
+					</tr>
+
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Amount</b></td>
+						<td>200.00</td>
+					</tr>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Date Income</b></td>
+						<td>2018-04-15</td>
+					</tr>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Captured By</b></td>
+						<td>Mashingaidze</td>
+					</tr>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Date Captured</b></td>
+						<td>2018-04-16 03:49:20</td>
+					</tr>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Administrator</b></td>
+						<td>Mashingaidze</td>
+					</tr>
+					<tr>
+						<td><b style="font-family:serif;font-size:15px">Comment</b></td>
+						<td></td>
+					</tr>
 
 
-	</table>
+					</tbody>
+
+
+				</table>
+			</div>
+		</div>
 
 	 </div>
     	
